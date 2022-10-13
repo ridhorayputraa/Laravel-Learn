f456a35345314da7f49725e2a0e817d55d023024
ridhorayputraa <ridhorayputra@gmail.com>
2022-10-13 17:22:30 +0700

    first commit

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+
+    @extends('layouts.main')
+
+    @section('container')
+    <h1>Hello, world!</h1>
+        
+    @endsection
+      
+
+    
+      
+
---
+++ resources/views/post.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('container')
+    
+<article>
+<h2>{{ $post->title }} </h2>
+
+{!! $post->body !!}
+
+
+
+</article>
+
+<a href="/posts">Back</a>
+@endsection
